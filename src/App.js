8e99ac8cff0c1e541821a7c6fd7563dc3782ec4c
labore <akodjolabore@gmail.com>
2022-10-28 14:59:52 +0000

    portfolio mui

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./components";
+import Resume from "./components/Resume";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Particles from "./Particles";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Particles />
+    </React.Fragment>
+  );
+};
+
+export default Home;
